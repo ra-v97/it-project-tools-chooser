d4ce637665adbdfd200d9cbc9dccac82b89a01f1
Kamil Doległo <kamilok1965@interia.pl>
2020-11-24 22:23:28 +0100

    Remove MainApp file

--- it-project-tools-chooser/src/main/kotlin/main.kt
+++ it-project-tools-chooser/src/main/kotlin/main.kt
@@ -1,7 +1,10 @@
 package pl.edu.agh.it.tools
 
+import pl.edu.agh.it.tools.views.MainView
+import tornadofx.App
 import tornadofx.launch
 
 
 fun main(args: Array<String>): Unit = launch<MainApp>(args)
 
+class MainApp : App(MainView::class)
